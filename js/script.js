296e8ed0e4f95ffaad6ec410d0fd37bb4c8b8761
NickusTop <148118087+NickusTop@users.noreply.github.com>
2024-11-18 18:17:09 +0200

    HW

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// 1
+let celsius = 25;
+let fahrenheit = celsius * 9/5 + 32;
+console,console.log(fahrenheit);
+// 2
+let day = 31;
+let hours = day * 24;
+let min = hours * 60;
+console.log("Кількість годин у місяці:", hours);
+console.log("Кількість хвилин у місяці:", min);
+// 3
+let health = 100;
+let energy = 50;
+health -= 75;
+energy -= 40;
+console.log("Рівень здоров'я у граця:", health);
+console.log("Рівень енергії у граця:", energy);
+// 4
+let totalAmount = 200;
+let znyzka = 0.10;
+let znyzkaAmount = totalAmount * (1 - znyzka);
+console.log("Сума після знижки:", znyzkaAmount);
+// 5
+const number = 1.5;
+const number2 = Math.floor(number);
+console.log(number2);
+// 6
+let numberKoma = "12.18";
+let numberNokoma = parseFloat(numberKoma);
+console.log(numberNokoma);
+// 7 
+let numberString = "22";
+let numberD = parseInt(numberString);
+console.log(numberD);
+// 8
+let numberS = 45;
+let sqrtnumber = Math.sqrt(numberS);
+console.log(sqrtnumber)
+// 9
+let numberG = 100;
+let numberStr = "200";
+let numberT = parseInt(numberStr);
+console.log(numberT);
+
+let stringNum = numberG.toString();
+console.log(stringNum)
+
+
+
+
+
+
